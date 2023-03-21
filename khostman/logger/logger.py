c289b0d985434d5d0ff42e66edb5340998edadd2
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-21 14:36:53 +0200

    Used OSUtils.get_project_root method

--- khostman/logger/logger.py
+++ khostman/logger/logger.py
@@ -1,12 +1,13 @@
 import logging
-from pathlib import Path
+
+from khostman.utils.os_utils import OSUtils
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s\t[%(levelname)s]\t%(message)s')
 
-project_root = Path(__file__).resolve().parents[2]
-logs_dir = Path(project_root) / 'logs'
+project_root = OSUtils.get_project_root()
+logs_dir = project_root / 'logs'
 
 if not logs_dir.exists():
     logs_dir.mkdir()
