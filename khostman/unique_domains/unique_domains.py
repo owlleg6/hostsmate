f929795469f9bec179e115d511131e72cbfca402
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-22 14:22:53 +0200

    Added docstrings and type hints to UniqueDomains

--- khostman/unique_domains/unique_domains.py
+++ khostman/unique_domains/unique_domains.py
@@ -1,8 +1,21 @@
 class UniqueDomains:
     unique_domains = set()
 
-    def add_domain(self, domain):
+    def add_domain(self, domain: str) -> None:
+        """Add a domain name to the set of unique domains.
+
+        Args:
+            domain (str): The domain name to add.
+
+        Returns:
+            None
+        """
         self.unique_domains.add(domain)
 
-    def get_unique_domains(self):
+    def get_unique_domains(self) -> set:
+        """Return the set of unique domain names.
+
+        Returns:
+            set: The set of unique domain names.
+        """
         return self.unique_domains
