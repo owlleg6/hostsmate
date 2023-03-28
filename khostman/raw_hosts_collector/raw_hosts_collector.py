8485f390580b61d67184e14477b6e7f0d0062854
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-28 18:07:57 +0300

    Renamed get_hosts_from_source to write_source_to_tmp method of the Writer class, refactored it

--- khostman/raw_hosts_collector/raw_hosts_collector.py
+++ khostman/raw_hosts_collector/raw_hosts_collector.py
@@ -46,6 +46,7 @@ class RawHostsCollector:
             response = get(url)
             response.raise_for_status()
             contents = response.text
+            logger.info(f'Successfully fetched contents of {url}')
             return contents
         except RequestException as e:
             logger.error(f'Could not fetch blacklisted domains from {url}: {e}')
@@ -53,14 +54,23 @@ class RawHostsCollector:
             return None
 
     @LoggingUtils.func_and_args_logging
-    def get_hosts_from_source(self, url, temp_file):
-        """ 
-        Gets the domains from a given source URL, and writes them to a temporary file 
+    def write_source_to_tmp(self, url: str, temp_file: str) -> None:
+        """Append contents of the given URL to the temporary file.
+           Get the source contents by calling fetch_source_contents method
+
+        Args:
+             url (str): the URL containing list of blacklisted domains.
+             temp_file (str): path to the temporary file
         """
         contents = self.fetch_source_contents(url)
+
         if contents is not None:
-            with open(temp_file, 'a') as f:
-                f.write(f'{contents}\n')
+            try:
+                with open(temp_file, 'a') as f:
+                    f.write(f'{contents}\n')
+                    logger.info(f'Successfully wrote contents of {url} to {temp_file}')
+            except OSError as e:
+                logger.error(f'Failed to write contents of {url} to {temp_file}: {e}')
 
     @LoggingUtils.func_and_args_logging
     def extract_raw_sources_contents(self, tmp: str):
@@ -72,7 +82,7 @@ class RawHostsCollector:
         with concurrent.futures.ProcessPoolExecutor() as executor:
             # iterate through blacklist sources and fetch hosts in parallel
             for source in self.blacklist_sources:
-                executor.submit(self.get_hosts_from_source, source, tmp)
+                executor.submit(self.write_source_to_tmp, source, tmp)
 
     def __repr__(self):
         return f'{__class__.__name__}'
