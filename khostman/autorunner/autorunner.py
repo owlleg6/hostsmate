2d50b5a9ff3b8924f85969989ee4b17779c2323c
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-29 22:09:14 +0300

    Refactored docstring and added type hints to variables of Autorunner class

--- khostman/autorunner/autorunner.py
+++ khostman/autorunner/autorunner.py
@@ -1,30 +1,44 @@
 import subprocess
+from pathlib import Path
 
 from khostman.cli.prompt import UserInteraction
 from khostman.utils.data_utils import OSUtils
 
 
 class Autorunner:
-    khostman_app = OSUtils.get_project_root() / 'app.py'
-    job_setter_sh = OSUtils.get_project_root() / 'anacron_job_setter.sh'
+    """A class responsible for setting up automatic update of the system's
+     Hosts file.
+
+    Attributes:
+        khostman_app (Path) = path to the khostman.py file.
+        job_setter_sh (Path) = path to the anacron_job_setter.sh which sets up
+        an anacron job for Linux (bash only).
+
+    """
+    khostman_app: Path = OSUtils.get_project_root() / 'app.py'
+    job_setter_sh: Path = OSUtils.get_project_root() / 'anacron_job_setter.sh'
 
     @staticmethod
     def check_anacron_dependency() -> None:
         """
-        Exit if anacron package is not installed.
+        Verify whether anacron package is installed on the system, exit if not.
         """
         anacron = subprocess.run(['which', 'anacron'], capture_output=True)
         if not anacron.stdout:
             exit("Please install 'anacron' dependency and try again.")
 
     def add_exec_permissions(self):
+        """Add executable permissions to the anacron job setter bash script."""
         subprocess.run(['chmod', '+x', self.job_setter_sh])
 
     def set_anacron_job(self):
+        """Run the anacron_job_setter.sh script."""
+
         self.check_anacron_dependency()
         autorun_frequency = UserInteraction.ask_autorun_frequency()
         self.add_exec_permissions()
         subprocess.run(['bash',
                         self.job_setter_sh,
                         autorun_frequency,
-                        f'python3 {self.khostman_app} --go'])
+                        f'python3 {self.khostman_app} --go'
+                        ])
