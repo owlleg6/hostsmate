de84ea9c8c4d3d38f2dbd780ffd934282bc1bb23
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-29 22:58:00 +0300

    Refactored docstrings and added type hints for variables in HostsFileUpdater class

--- khostman/hosts_file_updater/hosts_file_updater.py
+++ khostman/hosts_file_updater/hosts_file_updater.py
@@ -7,18 +7,22 @@ from khostman.writer.writer import Writer
 
 class HostsFileUpdater:
     """
-    Managing class that updates the system hosts file by downloading raw sources of
-    domain entries, formatting them, removing duplicates, and writing the
-    resulting entries, along with the header, to the hosts file.
+    Managing class that updates the system Hosts file by downloading raw
+    sources of domain entries, formatting them, removing duplicates, and
+    writing the resulting entries, along with the header, to the hosts file.
+
+    Attributes:
+        temp_file (str) path to temporary file for RawHostsCollector and
+        Formatter classes usage.
     """
-    tmp = OSUtils.mk_tmp_hex_file()
+    temp_file: str = OSUtils.mk_tmp_hex_file()
 
     def run(self) -> None:
         """
         Collects domain entries from raw sources, formats them, removes
         duplicates, and writes the resulting entries to the hosts file.
         """
-        RawHostsCollector().extract_raw_sources_contents(self.tmp)
-        Formatter().format_raw_lines(self.tmp)
+        RawHostsCollector().process_sources_concurrently(self.temp_file)
+        Formatter().format_raw_lines(self.temp_file)
         UniqueDomains().get_unique_domains()
         Writer().build_hosts_file()
