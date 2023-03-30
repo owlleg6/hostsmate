67f5a7687e16d38c753f0279c905127a3a567809
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-30 12:33:07 +0300

    Refactored docstrings, added type hints to varibles in Suspender class

--- khostman/suspender/suspender.py
+++ khostman/suspender/suspender.py
@@ -1,37 +1,43 @@
+from pathlib import Path
+
 from khostman.logger.logger import logger
 from khostman.utils.os_utils import OSUtils
 
 
 class Suspender:
     """
-    A class for suspending and resuming an adblocker by renaming the hosts file.
+    A class for suspending and resuming an adblocker by renaming the system's Hosts file.
+
+    Attributes:
+        org_hosts_name (Path): a path to the system's Hosts file.
+        renamed_hosts (Path): a path the renamed system's Hosts file (with tilda).
     """
 
-    org_hosts_name = OSUtils().path_to_hosts()
+    org_hosts_name: Path = OSUtils().path_to_hosts()
 
-    renamed_hosts = org_hosts_name.with_name(org_hosts_name.name + '~')
+    renamed_hosts: Path = org_hosts_name.with_name(org_hosts_name.name + '~')
 
     def suspend(self) -> None:
         """
-        Suspend the adblocking
+        Suspend the adblocking by renaming the Hosts file.
         """
         try:
             self.org_hosts_name.rename(
                 self.renamed_hosts)
             print("Adblocking is being suspended. Don't forget to enable it back!")
-            logger.debug(f'Adblocking has been suspended. "{self.org_hosts_name}" renamed to "{self.renamed_hosts}"')
+            logger.info(f'Adblocking has been suspended. "{self.org_hosts_name}" renamed to "{self.renamed_hosts}"')
         except FileNotFoundError:
-            logger.debug(f'Hosts file {self.org_hosts_name} was not found. Exiting')
+            logger.info(f'Hosts file {self.org_hosts_name} was not found. Exiting')
             exit(f'Hosts file {self.org_hosts_name} was not found.')
 
     def resume(self) -> None:
         """
-        Resume the adblocking
+        Resume the adblocking by renaming the Hosts file.
         """
         try:
             self.renamed_hosts.rename(self.org_hosts_name)
             print('Adblocker has been resumed')
-            logger.debug('Adblocker has been resumed')
+            logger.info('Adblocker has been resumed')
         except FileNotFoundError:
-            logger.debug(f'No {self.renamed_hosts} file was found')
+            logger.info(f'No {self.renamed_hosts} file was found')
             exit('Seems that the adblocker is running already.')
