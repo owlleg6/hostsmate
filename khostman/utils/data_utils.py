9ed9e0711b631bebdca9a7dd0b004f1b36de8259
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-03-30 13:20:26 +0300

    Refactored docstrings, added logging, added type hints to variables

--- khostman/utils/data_utils.py
+++ khostman/utils/data_utils.py
@@ -1,12 +1,16 @@
 import json
+from pathlib import Path
 
 from khostman.utils.utils import Utils
 from khostman.utils.os_utils import OSUtils
+from khostman.logger.logger import logger
+
 
 class DataUtils(Utils):
+    """A collection of utility methods for data processing."""
 
     @staticmethod
-    def extract_sources_from_json(whitelist=False, blacklist=False) -> list:
+    def extract_sources_from_json(whitelist=False, blacklist=False) -> list[str]:
         """
         Extracts a list of sources from a JSON file.
 
@@ -15,15 +19,21 @@ class DataUtils(Utils):
             blacklist (bool): Return blacklist sources.
 
         Returns:
-            list: A list of sources extracted from the JSON file.
+            list[str]: A list of sources extracted from the JSON file.
         """
-        resources_dir = OSUtils.get_project_root() / 'khostman' / 'resources'
+        resources_dir: Path = OSUtils.get_project_root() / 'khostman' / 'resources'
+
         if whitelist:
-            source_json = resources_dir / 'whitelist_sources.json'
+            source_json: Path = resources_dir / 'whitelist_sources.json'
         elif blacklist:
-            source_json = resources_dir / 'blacklist_sources.json'
+            source_json: Path = resources_dir / 'blacklist_sources.json'
+
+        else:
+            logger.error('No list type (blacklist or whitelist) was specified')
+            raise ValueError('"whitelist" or "blacklist" argument must be set to True')
 
         with open(source_json) as source:
-            contents = json.load(source)
-            sources = contents['sources']
+            contents: dict[str, list[str]] = json.load(source)
+            sources: list[str] = contents['sources']
+            logger.debug(f'Sources from {resources_dir} source were fetched')
             return sources
