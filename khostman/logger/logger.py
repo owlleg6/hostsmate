4fa527c15d0f1b06888b95ced680a830a2085a93
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-04-04 14:07:01 +0300

    Refactored HostsLogger class

--- khostman/logger/logger.py
+++ khostman/logger/logger.py
@@ -1,16 +1,47 @@
 import logging
 from pathlib import Path
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s\t[%(levelname)s]\t%(message)s')
+import khostman.utils.os_utils as utils
 
-project_root = Path(__file__).resolve().parents[2]
-logs_dir = project_root / 'logs'
 
-if not logs_dir.exists():
-    logs_dir.mkdir()
+class HostsLogger:
+    """
+    This class is used to create Logger instances.
+    """
 
-file_handler = logging.FileHandler(f'{project_root}/logs/hosts.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+    def create_logger(self, name: str) -> logging.Logger:
+        """
+        Create a logger object and return it.
+
+        Args:
+            name (str): The name of the logger.
+
+        Returns:
+            logging.Logger: The logger object.
+        """
+        logs_dir: Path = self.get_logs_dir()
+
+        logger: logging.Logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] '
+                                      '[%(name)s.%(funcName)s] - %(message)s')
+
+        file_handler = logging.FileHandler(f'{logs_dir}/hosts.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        return logger
+
+    @staticmethod
+    def get_logs_dir() -> Path:
+        """
+        Return the path to the directory where the log files will be stored.
+
+        Returns:
+            Path: The path to the logs directory.
+        """
+        logs_dir: Path = utils.OSUtils.get_project_root() / 'logs'
+
+        if not logs_dir.exists():
+            logs_dir.mkdir()
+        return logs_dir
