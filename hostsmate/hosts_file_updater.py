ad846e60fae5889e13fec4055579222e1f36ca64
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-04-13 16:05:40 +0300

    Changed call of RawHostsUpdater class process_sources_concurrently method to the BlacklistSources append_sources_contents_to_file_concurrently

--- hostsmate/hosts_file_updater.py
+++ hostsmate/hosts_file_updater.py
@@ -1,7 +1,6 @@
 from hostsmate.formatter import Formatter
-from hostsmate.raw_hosts_collector import RawHostsCollector
+from hostsmate.sources.blacklist_sources import BlacklistSources
 from hostsmate.utils.os_utils import OSUtils
-from hostsmate.utils.data_utils import DataUtils
 from hostsmate.system_hosts_file import SystemHostsFile
 
 
@@ -20,11 +19,9 @@ class HostsFileUpdater:
         duplicates, and writes the resulting entries to the hosts file.
         """
         temp_file: str = OSUtils().mk_tmp_hex_file()
-        blacklist_sources = DataUtils().extract_sources_from_json(blacklist=True)
 
-        RawHostsCollector().process_sources_concurrently(
-            temp_file,
-            blacklist_sources
+        BlacklistSources().append_sources_contents_to_file_concurrently(
+            temp_file
         )
         Formatter().format_raw_lines(temp_file)
         SystemHostsFile().build()
